1a07ff668d7a9c0efffbc7aa94b17691f7137afc
Maged <maged@Mageds-MBP.local>
2026-06-03 01:17:23 +0300

    exclude system accounts from audited by dropdown

--- src/pages/NewAuditPage.jsx
+++ src/pages/NewAuditPage.jsx
@@ -38,7 +38,7 @@ export default function NewAuditPage() {
   useEffect(() => {
     api.get('/employees?status=active').then(r => setEmployees(r.data)).catch(console.error);
     api.get('/ppe').then(r => setPpeItems(r.data)).catch(console.error);
-    api.get('/users').then(r => { setUsers(r.data); }).catch(console.error);
+    api.get('/users').then(r => { setUsers(r.data.filter(u => !['admin@egypro.com','sync@egypro.com'].includes(u.email))); }).catch(console.error);
   }, []);
 
   useEffect(() => {
